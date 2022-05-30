5d261fb95d5dac91e7b78d4f584864970d032c4d
Romeu Rezende <romeu_0104@hotmail.com>
2022-05-30 16:35:40 -0300

    Comentários finais desafio funções construtoras

--- desafios/Js/iniciante/11funcaoConstrutoraArray.js
+++ desafios/Js/iniciante/11funcaoConstrutoraArray.js
@@ -27,3 +27,17 @@ let view1 = QuantViews()
 let comentario1=new Comentarios("Romeu", "Teste")
 
 console.log(postagem1,view1,comentario1)
+
+
+// No desafio foi solicitado q colocasse contador de visualizações e comentários com mensagem e autor da msg, porém na realização do exercício não foi feito isso, ele criou uma função construtora com todos as propriedades pedidas, em visualizações e atribuiu "this.visualizações=0" e comentários "this.comentários=[]", após disse q deveria ser criada uma função a parte para essas e etc, mas n fez, por isso n tenho uma melhor solução q essa, a dele foi igual a abaixo:
+/*
+    function Postagem(tittle,post,author){
+    this.tittle=tittle
+    this.post=post
+    this.author=author
+    this.vizualizações=0;
+    this.comentarios=[]
+    this.estaOnline=false
+}
+
+OBS.: na hora q fiz o exercício esqueci do "está online ou não" porém faria seguindo a mesma linha de raciocínio da função QuantViews, mudando os valores para booleanos*/
